51b8f427b7871a301e8fc957a5dfba074a5873f6
jackjennings <jack@standard-library.com>
2019-07-18 16:52:47 -0700

    Update babel dependency for security issues

--- build/index.js
+++ build/index.js
@@ -3,37 +3,39 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports["default"] = void 0;
 
 var _kefir = require("kefir");
 
-var _hammerjs = require("hammerjs");
+var _hammerjs = _interopRequireDefault(require("hammerjs"));
 
-var _hammerjs2 = _interopRequireDefault(_hammerjs);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function observer(element) {
-  var manager = new _hammerjs2.default(element);
-
-  manager.get("swipe").set({ velocity: 0.1 });
-
+  var manager = new _hammerjs["default"](element);
+  manager.get("swipe").set({
+    velocity: 0.1
+  });
   return manager;
 }
 
 function surface(element) {
   var manager = observer(element);
+
   var swipe = _kefir.Kefir.fromEvents(manager, "swipe");
+
   var left = swipe.filter(function (e) {
     return e.direction == 2;
   });
   var right = swipe.filter(function (e) {
     return e.direction == 4;
   });
-
   swipe.left = left;
   swipe.right = right;
-
-  return { swipe: swipe };
+  return {
+    swipe: swipe
+  };
 }
 
-exports.default = surface;
+var _default = surface;
+exports["default"] = _default;
